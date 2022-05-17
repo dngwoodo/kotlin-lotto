160a244df9b458602ea2a63782b3f5824c251660
Molly <dngwoodo@gmail.com>
2022-05-18 01:02:50 +0900

    wip

---
+++ src/test/kotlin/CalculatorTest.kt
@@ -0,0 +1,8 @@
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class CalculatorTest : DescribeSpec({
+    describe("") {
+        it("")
+    }
+})
---
+++ src/test/kotlin/SeparatorTest.kt
@@ -0,0 +1,11 @@
+import io.kotest.core.spec.style.DescribeSpec
+
+class SeparatorTest : DescribeSpec({
+    context("구분자로 쉼표를 가질 때") {
+        it("구분자를 기준으로 분리한 각 숫자의 합을 반환한다.") {
+            Separator()
+        }
+    }
+
+
+})
